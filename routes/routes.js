e6eb8520144375d1d7e8ac9b4abe6b2ff82a2545
EdwardJoko <edwardjptatuhas@outlook.com>
2020-10-14 04:01:13 +0700

    Adding copy button

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const router  = express.Router();
 
-const Url = require('../models/Url');
+const Url    = require('../models/Url');
+const Clicks = require('../models/helloClick');
 
 router.get('/', async (req, res) => {
-  const Urls = await Url.find();
-  res.render('index', { Urls: Urls});
+  const Urls   = await Url.find();
+  const clicks = await Clicks.findOne({ short: "hello" });
+
+  res.render('index', { Urls: Urls, Clicks: clicks });
 });
 
 router.post('/shortUrls', async (req, res) => {
   await Url.create({ full: req.body.fullUrl });
 
   res.redirect('/');
+});
+
+router.get('/hello', async (req, res) => {
+  const clicks = await Clicks.findOne({ short: "hello" });
+  
+  clicks.clicks++;
+  clicks.save();
+
+  res.render('hello');
 });
 
 router.get('/:shortUrl', async (req, res) => {
